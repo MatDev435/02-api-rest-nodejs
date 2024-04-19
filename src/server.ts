507b11af680adfddcf13164b881007f8f1b6ef9a
MatDev435 <matheuspsalmazo2@gmail.com>
2024-04-18 22:15:11 -0300

    Banco de dados - Tratando env com zod

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { randomUUID } from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transactions
 })
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('🚀 HTTP Server Running!')
 })
