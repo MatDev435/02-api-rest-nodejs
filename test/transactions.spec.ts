9c59044c9b272496f7503f096aef3d0dfc06d0cc
MatDev435 <matheuspsalmazo2@gmail.com>
2024-04-23 11:40:29 -0300

    Testes E2E - Configurando banco de dados de testes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { beforeAll, afterAll, it, describe, expect } from 'vitest'
+import { beforeAll, beforeEach, afterAll, it, describe, expect } from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(() => {
+    execSync('pnpm knex migrate:rollback --all')
+    execSync('pnpm knex migrate:latest')
   })
 
   it('should be able to create a new transaction', async () => {
