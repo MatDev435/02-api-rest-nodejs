6d0402b36a41bf2a873fc57d2ec0d68c68aa09cd
MatDev435 <matheuspsalmazo2@gmail.com>
2024-04-23 09:31:20 -0300

    Testes E2E - Supertest configurado e teste de cadastro de transações realizado

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,5 @@
-import fastify from 'fastify'
+import { app } from './app'
 import { env } from './env'
-
-import { transactionsRoute } from './routes/transactions'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionsRoute, {
-  prefix: 'transactions',
-})
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('🚀 HTTP Server Running!')
--- test/exmaple.spec.ts
+++ test/exmaple.spec.ts
@@ -1,7 +1,22 @@
-import { expect, test } from 'vitest'
+import { beforeAll, afterAll, test } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('O usuário deve poder criar uma transação', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('user can create a transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
